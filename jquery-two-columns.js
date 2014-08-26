d9981ba2625a9a8cff28c2de5709706e031a2037
Ren Shao <ren@shao.io>
2014-08-26 12:56:04 +1000

    First cut. Set the width of left and right columns, set margin-left of right panel to produce gutter

---
+++ jquery-two-columns.js
@@ -0,0 +1,29 @@
+/*
+  Split a panel into two columns with gutter.
+  Specify width of columns in 'columns attribute'.
+  Example:
+
+  <div id="container" columns="70|25">
+    <article> ... </article>
+    <aside> ... </aside>
+  </div>
+
+  <script>
+    $('#container').twoColumns();
+  </script>
+*/
+
+$.fn.twoColumns = function() {
+  var columnWidths = this.attr('columns').split(/\|/);
+  var left = columnWidths[0], right = parseInt(columnWidths[1]);
+  var gutter = 100 - left - right;
+    
+  this.children().css('float', 'left');
+  this.children(':first').css('width', left + '%');
+  this.children(':last').css({
+    'width': right + '%',
+    'margin-left': gutter + '%'
+  });
+
+  return this;
+};
